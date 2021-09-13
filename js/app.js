ea0a3cc0c7cd0d7632cbd05a01d6810341245c9f
tahsinnur <tahsinnurtajin@gmail.com>
2021-09-13 16:27:07 +0600

    4th commit

--- js/app.js
+++ js/app.js
@@ -10,21 +10,21 @@ loadProducts();
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
+    const image = product.image;
     const div = document.createElement("div");
     div.classList.add("col");
     div.innerHTML = `
     <div class="card h-100 single-product">
-      <div class="text-center">
-      <img src="${product.image}" class="product-image pt-2" alt="...">
+      <img src="${image}" class="card-img-top product-image" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${product.title}</h5>
+        <p class="card-text">Category: ${product.category}</p>
+        <h6 class="card-text">Price: $${product.price}</h6>
+        <p>Product Rating: <span class="rating-color">${product.rating.rate}</span> <span class="reviewer-count-color">(${product.rating.count})</span></p>
       </div>
-      <div class="card-body text-center">
-        <h4>${product.title}</h4>
-        <p>Category: ${product.category}</p>
-        <h4>Price: $ ${product.price}</h4>
-        <p>Product Rating: <span class="rating-color">${product.rating.rate}</span> (Reviewers: <span class="reviewer-count-color">${product.rating.count}</span>)</p>
-        <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">Add To Cart</button>
-        <button id="details-btn" class="btn btn-danger">Details</button>
+      <div class="card-footer d-flex justify-content-evenly bg-white border-0">
+        <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-secondary">Add To Cart</button>
+        <button id="details-btn" class="btn btn-warning">Details</button></div>
       </div>
     </div>
     `;
@@ -84,3 +84,19 @@ const updateTotal = (id) => {
     getInputValue("total-tax");
   document.getElementById("total").innerText = parseFloat( grandTotal).toFixed(2);
 };
+
+const showStarRating = (rate) =>{
+  const rating = document.getElementById('rating');
+  const ul = document.createElement('ul');
+  ul.classList.add('star-list')
+  if(rate === 3.9){
+    ul.innerHTML = `
+    <li><i class="fas fa-star text-warning"></i></li>
+    <li><i class="fas fa-star text-warning"></i></li>
+    <li><i class="fas fa-star text-warning"></i></li>
+    <li><i class="fas fa-star text-warning"></i></li>
+    <li><i class="fas fa-star text-warning"></i></li>
+    `;
+  }
+  rating.appendChild(ul);
+}
